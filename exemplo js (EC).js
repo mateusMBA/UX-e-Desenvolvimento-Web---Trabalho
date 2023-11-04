3f2fa4ee28b70078cd9abfa25cd50ef6d12cfc4a
barretonath <84478377+barretonath@users.noreply.github.com>
2023-11-04 17:44:57 -0300

    Add files via upload

---
+++ exemplo js (EC).js
@@ -0,0 +1,5 @@
+// script.js
+function exibirTexto() {
+    var textoInserido = document.getElementById("texto").value;
+    document.getElementById("textoExibido").innerHTML = "Texto inserido: " + textoInserido;
+}
